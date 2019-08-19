66afac02eb39520ae759fd3e0d65e94f5e331998
Junbo <jqia0016@student.monash.edu>
2019-08-19 13:41:34 +1000

    weekly studio
    weekly studio needed

---
+++ week2/FIT5032WEEK2_databaseFirst/FIT5032WEEK2_databaseFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032WEEK2_databaseFirst.MvcApplication" Language="C#" %>
---
+++ week2/FIT5032WEEK2_databaseFirst/FIT5032WEEK2_databaseFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032WEEK2_databaseFirst.Startup))]
+namespace FIT5032WEEK2_databaseFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
